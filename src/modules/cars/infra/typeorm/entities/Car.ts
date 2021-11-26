5c434e3ea4c3bad812cad9f2e03bfd726d89aaaf
Henrique Costa dos Santos <Henrique.Santos@tutiplast.local>
2021-11-26 13:49:32 -0400

    Tabela de specifications_cars

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -3,12 +3,15 @@ import {
   CreateDateColumn,
   Entity,
   JoinColumn,
+  JoinTable,
+  ManyToMany,
   ManyToOne,
   OneToMany,
   PrimaryColumn,
 } from "typeorm";
 import { v4 as uuid } from "uuid";
 import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
@@ -37,11 +40,22 @@ class Car {
   brand: string;
 
   @ManyToOne(() => Category)
-  @JoinColumn({ name: "category_id" })
+  @JoinColumn({ name: "category_id" }) // o valor que será comparado no category_id
   category: Category;
 
   @Column()
   category_id: string;
+
+  // Entra na tabela specifications_Cars
+  // faz a relação da tabela car_id (id da tabela que ela pertence)
+  // e faz a relação de volta com a outa tabela com o inverseJoinColumns
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: "specifications_cars",
+    joinColumns: [{ name: "car_id" }],
+    inverseJoinColumns: [{ name: "specification_id" }],
+  })
+  specifications: Specification[];
 
   @CreateDateColumn()
   created_at: Date;
